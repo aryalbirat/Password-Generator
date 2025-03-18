a79bc04eeacc4f65ef005c0c6d337a8b410ea8d1
Birat <itsbirataryal+github@gmail.com>
2025-03-18 18:31:54 +0545

    Some changes to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useCallback, useEffect, useRef } from "react";
 
+//main app component
 function App() {
   const [length, setLength] = useState(8)
   const [numberAllowed, setNumberAllowed] = useState(false);
